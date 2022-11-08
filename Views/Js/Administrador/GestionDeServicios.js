61b1badf72ddcd2804f90173a2ee7ef803f7b9a4
Sebastian Santis <sebastiandasahe_1287@hotmail.com>
2022-11-08 17:18:34 -0500

    Quitando maquinas y tipo de servicio en gestion de servicios

--- Views/Js/Administrador/GestionDeServicios.js
+++ Views/Js/Administrador/GestionDeServicios.js
@@ -5,20 +5,17 @@ let tr_id = 0
 const guardarDocumento=()=>{
 
     let categoriasText = document.getElementById("categoriasText").value
-    let tiposText = document.getElementById("tiposText").value
     let descripcionText = document.getElementById("descripcionText").value
     
     let elementosText = document.getElementById("elementosText").value
     
-    let maquinasText = document.getElementById("maquinasText").value
     let trid = tr_id
     
     let costoElementoText = parseFloat(document.getElementById("costoElementoText").value)
-    let costoMaquinaText = parseFloat(document.getElementById("costoMaquinaText").value)
     let porcentajeGanancia = parseFloat(document.getElementById("porcentajeGanancia").value)
 
-    let ptotal = ((costoElementoText+costoMaquinaText)*(porcentajeGanancia/100))+(costoElementoText+costoMaquinaText)
-    let ctotal = costoElementoText+costoMaquinaText
+    let ptotal = ((costoElementoText)*(porcentajeGanancia/100))+(costoElementoText)
+    let ctotal = costoElementoText
         
     /*Tabla*/
     let tbody = document.getElementById("serviciosCreados")
@@ -28,17 +25,11 @@ const guardarDocumento=()=>{
     let td_1 = document.createElement('td')
     td_1.innerHTML = categoriasText
 
-    let td_2 = document.createElement('td')
-    td_2.innerHTML = tiposText
-
     let td_3 = document.createElement('td')
     td_3.innerHTML = descripcionText
 
     let td_4 = document.createElement('td')
     td_4.innerHTML=elementosText
-
-    let td_5 = document.createElement('td')
-    td_5.innerHTML = costoMaquinaText
 
     let td_6 = document.createElement('td')
     td_6.innerHTML = ctotal
@@ -53,7 +44,7 @@ const guardarDocumento=()=>{
     let buttonActualizar = document.createElement('button')
     buttonActualizar.innerHTML = 'Actualizar'
     buttonActualizar.addEventListener('click',()=>
-        actualizarDocumento(categoriasText,tiposText,descripcionText,elementosText,maquinasText,costoElementoText,costoMaquinaText,porcentajeGanancia,trid)
+        actualizarDocumento(categoriasText,descripcionText,elementosText,costoElementoText,porcentajeGanancia,trid)
     )
 
     let buttonEliminar = document.createElement('button')
@@ -62,18 +53,15 @@ const guardarDocumento=()=>{
 
     td_9.append(buttonActualizar,buttonEliminar)
 
-    tr.append(td_1,td_2,td_3,td_4,td_5,td_6,td_7,td_8,td_9)
+    tr.append(td_1,td_3,td_4,td_6,td_7,td_8,td_9)
     tbody.append(tr)
 
     tr_id++;
 
     document.getElementById("categoriasText").value = ""
-    document.getElementById("tiposText").value = ""
     document.getElementById("descripcionText").value = ""
     document.getElementById("elementosText").value = ""
-    document.getElementById("maquinasText").value = ""
     document.getElementById("costoElementoText").value = ""
-    document.getElementById("costoMaquinaText").value = ""
     document.getElementById("porcentajeGanancia").value = ""
     document.getElementById("precioTotal").value = ""
     document.getElementById("costoTotalText").value = ""
@@ -83,26 +71,22 @@ const guardarDocumento=()=>{
 const onCostChange = () =>{
 
     let costoElementoText = parseFloat(document.getElementById("costoElementoText").value)
-    let costoMaquinaText = parseFloat(document.getElementById("costoMaquinaText").value)
     let porcentajeGanancia = parseFloat(document.getElementById("porcentajeGanancia").value)
 
-    let ptotal = ((costoElementoText+costoMaquinaText)*(porcentajeGanancia/100))+(costoElementoText+costoMaquinaText)
-    let ctotal = costoElementoText+costoMaquinaText
+    let ptotal = ((costoElementoText)*(porcentajeGanancia/100))+(costoElementoText)
+    let ctotal = costoElementoText
 
     document.getElementById("precioTotal").value = ptotal
     document.getElementById("costoTotalText").value = ctotal
 
 }
 
-const actualizarDocumento=(categoriasText,tiposText,descripcionText,elementosText,maquinasText,costoElementoText,costoMaquinaText,porcentajeGanancia,trid)=>{
+const actualizarDocumento=(categoriasText,descripcionText,elementosText,costoElementoText,porcentajeGanancia,trid)=>{
 
     document.getElementById("categoriasText").value = categoriasText
-    document.getElementById("tiposText").value = tiposText
     document.getElementById("descripcionText").value = descripcionText
     document.getElementById("elementosText").value = elementosText
-    document.getElementById("maquinasText").value = maquinasText
     document.getElementById("costoElementoText").value = costoElementoText
-    document.getElementById("costoMaquinaText").value = costoMaquinaText
     document.getElementById("porcentajeGanancia").value = porcentajeGanancia
 
     onCostChange()
